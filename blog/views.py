0bf612354d1c0015f038a3ba95f7a9b987973213
MysteryCoder456 <rehatbir.singh@gmail.com>
2020-05-30 13:36:31 +0400

    Added simple home page

--- blog/views.py
+++ blog/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("<h1>Coming Soon!</h1>")
+    return render(request, "blog/home.html", {})
