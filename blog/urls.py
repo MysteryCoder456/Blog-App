8cfdbacdb756193903789a794f590abe8f1f03a8
MysteryCoder456 <rehatbir.singh@gmail.com>
2020-06-01 01:59:26 +0400

    Added New Blogs Page

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("blogs/", views.blogs, name="new_blogs")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import BlogList
+
 
 def home(request):
-    return render(request, "blog/home.html", {})
+    return render(request, "blog/home.html")
+
+
+def blogs(request):
+    bl = BlogList.objects.get(name="Technology")
+    return render(request, "blog/blogs.html", {"bloglist": bl})
