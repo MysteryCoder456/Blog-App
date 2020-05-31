30739369527979a9c6be6fc37de86e54d17ad02c
MysteryCoder456 <rehatbir.singh@gmail.com>
2020-05-31 11:03:15 +0400

    Added database models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.core.validators import MaxLengthValidator
+from django.utils import timezone
 
-# Create your models here.
+
+class BlogList(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Blog(models.Model):
+    blog_list = models.ForeignKey(BlogList, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=50)
+    content = models.TextField(
+        max_length=250,
+        blank=True,
+        validators=[MaxLengthValidator(250)]
+    )
+    creationDate = models.DateTimeField(default=timezone.now(), blank=True)
