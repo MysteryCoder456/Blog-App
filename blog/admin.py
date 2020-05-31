fa30f778dccf9246bc718b630459284a3db9b41f
MysteryCoder456 <rehatbir.singh@gmail.com>
2020-06-01 01:58:22 +0400

    fixed models

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import BlogList, Blog
 
-# Register your models here.
+admin.site.register(BlogList)
+admin.site.register(Blog)
--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MaxLengthValidator
+from django.utils import timezone
 
 
 class BlogList(models.Model):
@@ -13,9 +14,12 @@ class Blog(models.Model):
     blog_list = models.ForeignKey(BlogList, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=50)
-    creationDate = models.DateTimeField()
+    creation_date = models.DateTimeField(default=timezone.now())
     content = models.TextField(
         max_length=500,
         blank=True,
         validators=[MaxLengthValidator(500)]
     )
+
+    def __str__(self):
+        return self.title + " " + str(self.id)
