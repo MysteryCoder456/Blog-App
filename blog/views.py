4b987f4ae9d6ffd6bc006aa83502d1a1c918f9ee
MysteryCoder456 <rehatbir.singh@gmail.com>
2020-06-03 17:03:43 +0400

    Fixed glitch in the anti-spam system

--- blog/views.py
+++ blog/views.py
@@ -4,8 +4,10 @@ from .models import BlogList
 from .forms import NewBlogForm
 
 # Get the time of when the user opens homepage of website
-visit_time = int(timezone.now().time().strftime("%M"))
-print(visit_time)
+visit_hours = int(timezone.now().time().strftime("%H"))
+visit_minutes = int(timezone.now().time().strftime("%M"))
+visit_minutes_total = visit_hours * 60 + visit_minutes
+print(visit_minutes_total)
 
 
 def home(request):
@@ -18,27 +20,34 @@ def blogs(request):
 
 
 def create_blog(request):
-    global visit_time
+    global visit_hours, visit_minutes, visit_minutes_total
 
     # Get the current time and the time difference
-    current_time = int(timezone.now().time().strftime("%M"))
-    time_diff = current_time - visit_time
+    current_hours = int(timezone.now().time().strftime("%H"))
+    current_minutes = int(timezone.now().time().strftime("%M"))
+    current_minutes_total = current_hours * 60 + current_minutes
+    time_diff = current_minutes_total - visit_minutes_total
 
-    print(current_time, visit_time)
+    # TODO: Remove the below line before commiting
+    print(current_minutes_total, visit_minutes_total)
 
     if request.method == "POST":
         form = NewBlogForm(request.POST)
 
+        # Check if form is valid
         if form.is_valid():
+            # Get the relevant blog list
             blog_list = BlogList.objects.get(
                 name=form.cleaned_data["blog_list"]
             )
 
+            # Get the information from the form
             title = form.cleaned_data["title"]
             author = form.cleaned_data["author"]
             creation_date = form.cleaned_data["creation_date"]
             content = form.cleaned_data["content"]
 
+            # Create the Blog and save it
             b = blog_list.blog_set.create(
                 title=title,
                 author=author,
@@ -47,7 +56,11 @@ def create_blog(request):
             )
             b.save()
 
-            visit_time = int(timezone.now().time().strftime("%M"))
+            # Reset Visit Minutes to current time
+            visit_hours = current_hours
+            visit_minutes = current_minutes
+            visit_minutes_total = visit_hours * 60 + visit_minutes
+
             return redirect("/blogs")
 
     else:
