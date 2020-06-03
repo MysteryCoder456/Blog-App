743896821ef2d212b35fa11dfc371669aaa3ded5
MysteryCoder456 <rehatbir.singh@gmail.com>
2020-06-03 12:33:20 +0400

    Added anti-spam

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, redirect
+from django.utils import timezone
 from .models import BlogList
 from .forms import NewBlogForm
+
+# Get the time of when the user opens homepage of website
+visit_time = int(timezone.now().time().strftime("%M"))
+print(visit_time)
 
 
 def home(request):
@@ -13,11 +18,22 @@ def blogs(request):
 
 
 def create_blog(request):
+    global visit_time
+
+    # Get the current time and the time difference
+    current_time = int(timezone.now().time().strftime("%M"))
+    time_diff = current_time - visit_time
+
+    print(current_time, visit_time)
+
     if request.method == "POST":
         form = NewBlogForm(request.POST)
 
         if form.is_valid():
-            blog_list = BlogList.objects.get(name=form.cleaned_data["blog_list"])
+            blog_list = BlogList.objects.get(
+                name=form.cleaned_data["blog_list"]
+            )
+
             title = form.cleaned_data["title"]
             author = form.cleaned_data["author"]
             creation_date = form.cleaned_data["creation_date"]
@@ -31,7 +47,15 @@ def create_blog(request):
             )
             b.save()
 
+            visit_time = int(timezone.now().time().strftime("%M"))
+            return redirect("/blogs")
+
     else:
         form = NewBlogForm()
 
-    return render(request, "blog/create.html", {"form": form})
+    context = {
+        "form": form,
+        "time_diff": time_diff,
+        "time_left": 5 - time_diff
+    }
+    return render(request, "blog/create.html", context)
